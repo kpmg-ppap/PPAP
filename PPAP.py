d1cc7056a43d99b3e98557d042660c2df4392219
ayoungbyun <54729850+ayoungbyun@users.noreply.github.com>
2020-02-21 17:41:03 +0900

    Add files via upload

--- PPAP.py
+++ PPAP.py
@@ -3,14 +3,21 @@ from PyQt5.Qt import *
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
 from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QDesktopServices
+# from PyQt5.QtCore import QUrl
+# from PyQt5.QtWebEngineWidgets import QWebEngineView
+
 
 #UI파일은 Python 코드 파일과 같은 디렉토리에 위치
 form_class = uic.loadUiType("PPAP_UI.ui")[0]
 
 class MyWindow(QMainWindow, form_class):
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.search_hyperlink.linkActivated.connect(self.link)
 
         self.sum_btn.clicked.connect(self.summary)
         self.sum_input.setTextColor(QColor(0, 0, 0))
@@ -19,9 +26,10 @@ class MyWindow(QMainWindow, form_class):
         self.clm_btn.setVisible(False)
         self.clm_input.setTextColor(QColor(0, 0, 0))
 
-        self.search_btn.clicked.connect(self.search)
+        # self.search_btn.clicked.connect(self.search)
         self.search_btn.setVisible(False)
         self.search_output.setVisible(False)
+        self.search_hyperlink.setVisible(False)
         self.reset_btn.clicked.connect(self.reset)
 
         self.title_btn.clicked.connect(self.title)
@@ -30,15 +38,19 @@ class MyWindow(QMainWindow, form_class):
         self.title_text.setFontPointSize(20)
         self.title_text.insertPlainText("제목 없는 문서")
         self.title_list.setVisible(False)
-        MyWindow.initial_clipboard_len = len(QApplication.clipboard().text())
+
+    def link(self, linkStr):
+        QDesktopServices.openUrl(QUrl(linkStr))
+
+    def show_output(self, where, what):
+        where.clear()
+        where.insertPlainText("[검색 텍스트]")
+        where.append(what)
+        where.append("")
+        where.append("")
 
     def summary(self):
-        # if MyWindow.initial_clipboard_len != 0 :
-            # 일단 만약 기존 클립보드에 뭐가 있으면 지워
         QApplication.clipboard().clear()
-        # else :
-        #     # 없으면 패스
-        #     pass
 
         # 만약 선택영역이 있으면 복사가 될거야
         self.sum_input.copy()
@@ -46,7 +58,7 @@ class MyWindow(QMainWindow, form_class):
         if len(QApplication.clipboard().text()) == 0 :
             # 만약 클립보드에 아무것도 없으면 input 박스에 있는 텍스트 전체를 인풋으로 받아라
             text = self.sum_input.toPlainText()
-            self.sum_output.append(text)
+            self.show_output(self.sum_output, text)
             self.clm_btn.setVisible(True)
             self.title_btn.setVisible(True)
             QApplication.clipboard().clear()
@@ -57,7 +69,8 @@ class MyWindow(QMainWindow, form_class):
             self.search_output.setVisible(True)
             self.sum_input.setTextBackgroundColor(QColor(85, 170, 0))
             text = QApplication.clipboard().text()
-            self.search_output.append(text)
+            self.show_output(self.search_output, text)
+            self.search(text)
             self.sum_input.moveCursor(QTextCursor.Right)
             self.sum_input.setTextBackgroundColor(QColor(255, 255, 255))
             QApplication.clipboard().clear()
@@ -71,7 +84,7 @@ class MyWindow(QMainWindow, form_class):
         if len(QApplication.clipboard().text()) == 0:
             # 만약 클립보드에 아무것도 없으면 input 박스에 있는 텍스트 전체를 인풋으로 받아라
             text = self.clm_input.toPlainText()
-            self.clm_output.append(text)
+            self.show_output(self.clm_output, text)
             self.clm_btn.setVisible(True)
             self.title_btn.setVisible(True)
             QApplication.clipboard().clear()
@@ -82,26 +95,21 @@ class MyWindow(QMainWindow, form_class):
             self.search_output.setVisible(True)
             self.clm_input.setTextBackgroundColor(QColor(255, 106, 101))
             text = QApplication.clipboard().text()
-            self.search_output.append(text)
+            self.show_output(self.search_output, text)
+            self.search(text)
             self.clm_input.moveCursor(QTextCursor.Right)
             self.clm_input.setTextBackgroundColor(QColor(255, 255, 255))
             QApplication.clipboard().clear()
 
 
-    def search(self):
-        pass
+    def search(self, keyword):
         # google search
+        self.search_hyperlink.setVisible(True)
+        self.search_keyword = keyword
+        self.search_hyperlink.setText('<a href="http://www.google.com/search?q='+ self.search_keyword +'">"'+ self.search_keyword +'" Google 검색</a>')
 
-        # self.output_text.clear()
-        # exp1 = self.exp_in.toPlainText()
-        # ctx1 = self.ctx_in.toPlainText()
-        # self.output_text.insertPlainText("[표현검색 텍스트]")
-        # self.output_text.append(exp1)
-        # self.output_text.append("")
-        # self.output_text.append("")
-        # self.output_text.insertPlainText("[내용검색 텍스트]")
-        # self.output_text.append(ctx1)
-        # self.output_text.append("")
+    def show_search_result(self):
+        pass
 
     def reset(self):
         clm_original = self.clm_input.toPlainText()
@@ -121,9 +129,9 @@ class MyWindow(QMainWindow, form_class):
     def title(self):
         self.title_list.clear()
         self.title_list.setVisible(True)
-        self.title_list.addItem("후보1")
-        self.title_list.addItem("후보2")
-        self.title_list.addItem("후보3")
+        title_rcmd = ["후보1", "후보2", "후보3"]
+        for title in title_rcmd:
+            self.title_list.addItem(title)
         self.title_list.itemClicked.connect(self.chkItemClicked)
 
     def chkItemClicked(self):
